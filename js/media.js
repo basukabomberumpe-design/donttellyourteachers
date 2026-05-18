bc77ad5d4cc4ed6827c93cc7ace0ce9a15ad4566
happyrilla <basukabomberumpe@gmail.com>
2026-05-18 10:53:49 +0200

    better code

--- js/media.js
+++ js/media.js
@@ -1,6 +1,6 @@
 let storage = localStorage.getItem("media");
-if (storage === null) {
-    console.log("if you want you can change the media tab to be my collection of videos in settings");
+if (storage === null || storage === "null") {
+  console.log("if you want you can change the media tab to be my collection of videos in settings");
 } else {
-    window.location.href = "/the-hub/sub-sites/mediac.html";
+  window.location.href = "/the-hub/sub-sites/mediac.html";
 }
