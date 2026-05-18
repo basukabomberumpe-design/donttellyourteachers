332edc8a7c349b56ae4b1b2fc90f3474b689c7f6
happyrilla <basukabomberumpe@gmail.com>
2026-05-18 09:39:31 +0200

    start mediac

---
+++ js/media.js
@@ -0,0 +1,6 @@
+let storage = localStorage.getItem(media)
+if (storage === null) {
+    console.log("if you want you can change the media tab to be my collection of videos in settings")
+} else {
+    window.location.href = "/the-hub/sub-sites/mediac.html"
+}
