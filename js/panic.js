a0eb20ff34ac68c5dc8f2226938a7eb0c4393149
happyrilla <basukabomberumpe@gmail.com>
2026-05-19 12:03:12 +0200

    panic

--- js/panic.js
+++ js/panic.js
@@ -1,6 +1,6 @@
 (function() {
-  const b = localStorage.getItem("panicbutton");
-  const l = localStorage.getItem("paniclink");
+  const b = localStorage.getItem("panicbutton") || "p";
+  const l = localStorage.getItem("paniclink") || "https://www.google.com";
 
   window.addEventListener("keydown", e => {
     if (e.key === b) window.location.href = l;
