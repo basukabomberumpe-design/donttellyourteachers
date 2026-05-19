a9b6e5234e78fe7828f47892557ded2a77f6ad27
happyrilla <basukabomberumpe@gmail.com>
2026-05-19 12:16:24 +0200

    ok

--- js/panic.js
+++ js/panic.js
@@ -1,6 +1,6 @@
 (function() {
   const b = (localStorage.getItem("panicbutton") || "p").toLowerCase();
-  const l = localStorage.getItem("paniclink") || "https://www.google.com";
+  const l = localStorage.getItem("paniclink");
 
   window.addEventListener("keydown", e => {
     if (e.key.toLowerCase() === b) window.location.href = l;
