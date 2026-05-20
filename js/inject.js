40e7cc1f1dfc8e74799aab24db8dda778b51ac7f
happyrilla <basukabomberumpe@gmail.com>
2026-05-20 08:34:54 +0200

    better js

--- js/inject.js
+++ js/inject.js
@@ -1,9 +1,15 @@
-const i3 = document.getElementById("3");
-const i4 = document.getElementById("4");
+document.addEventListener('DOMContentLoaded', () => {
+  const i3 = document.getElementById("3");
+  const i4 = document.getElementById("4");
 
-const l3 = localStorage.getItem("panicbutton");
-const l4 = localStorage.getItem("paniclink");
+  const l3 = localStorage.getItem("panicbutton") ?? '';
+  const l4 = localStorage.getItem("paniclink") ?? '';
 
+  if (i3) {
+    i3.textContent = l3;
+  }
 
-i3.innertext = l3
-i4.innertext = l4
+  if (i4) {
+    i4.textContent = l4;
+  }
+});
