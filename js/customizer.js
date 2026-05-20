b3c5ede1ffb641a0b76e20e1c76c4798c3b1b5ec
happyrilla <basukabomberumpe@gmail.com>
2026-05-20 09:19:39 +0200

    added icon changer

--- js/customizer.js
+++ js/customizer.js
@@ -1,8 +1,15 @@
 let idk = localStorage.getItem("title")
+let favi = localStorage.getItem("favicon")
 function setTitle(value) {document.title = value || "Google";}
+function setfavicon(icon) {document.querySelector("link[rel='icon']").href = icon;}
 if (idk === null) {
     setTitle("Google")
 } else {
     setTitle(idk);
     console.log("the title set is:", idk, "you can change it in the settings");
+}
+if (favi === null) {
+    setfavicon("https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico");
+} else {
+    setfavicon("favi")
 }
