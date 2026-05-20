260478205ca7b49ee94d5f11a90f5d42459c63bf
happyrilla <basukabomberumpe@gmail.com>
2026-05-20 08:38:26 +0200

    hopefully works

--- js/inject.js
+++ js/inject.js
@@ -2,14 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const i3 = document.getElementById("3");
   const i4 = document.getElementById("4");
 
-  const l3 = localStorage.getItem("panicbutton") ?? '';
-  const l4 = localStorage.getItem("paniclink") ?? '';
+  const l3 = localStorage.getItem("panicbutton");
+  const l4 = localStorage.getItem("paniclink");
 
-  if (i3) {
-    i3.textContent = l3;
-  }
+  console.log('panicbutton:', l3, 'paniclink:', l4);
 
-  if (i4) {
-    i4.textContent = l4;
-  }
+  if (i3) i3.textContent = l3 ?? '';
+  if (i4) i4.textContent = l4 ?? '';
 });
